a461c9927aa7abeb913b4dbab9ec723113e4a71d
infomani23 <dbamanikandan23@gmail.com>
2023-09-21 18:22:49 +0530

    create register form

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById("parent").addEventListener("click", handleClick)
+  
+//   document.getElementById("child").addEventListener("click", handleClick)
+ 
+//   document.getElementById("grandchild").addEventListener("click", handleClick)  
+ 
+//   function handleClick(event) {
+//     const elementClicked = event.target;
+//     console.log(`${elementClicked.tagName} clicked!`);
+//   }
